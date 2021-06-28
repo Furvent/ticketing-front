2faae615635c366baebda38ea6b6d2259e37c698
Furvent <furventt@gmail.com>
2021-06-28 09:24:53 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestingServicesComponent } from './tests/testing-services/testing-services.component';
+import { ConnectionComponent } from './views/connection/connection/connection.component';
+import { GeneralDashboardComponent } from './views/general-dashboard/general-dashboard/general-dashboard.component';
+import { GroupDashboardComponent } from './views/group-dashboard/group-dashboard/group-dashboard.component';
 
 const routes: Routes = [
-  {path: 'tests', component: TestingServicesComponent}
+  {path: 'tests', component: TestingServicesComponent},
+  {path: 'connection', component: ConnectionComponent},
+  {path: 'general-dashboard', component: GeneralDashboardComponent},
+  {path: 'group-dashboard', component: GroupDashboardComponent},
+  {path: '', redirectTo: 'connection', pathMatch: 'full'},
 ];
 
 @NgModule({
