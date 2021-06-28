0b8ecbc602ed37b55be70de2bd1bf9afc1ebcd46
Furvent <furventt@gmail.com>
2021-06-28 09:15:48 +0200

    Add three components view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TestingServicesComponent } from './tests/testing-services/testing-services.component';
+import { ConnectionComponent } from './views/connection/connection/connection.component';
+import { GeneralDashboardComponent } from './views/general-dashboard/general-dashboard/general-dashboard.component';
+import { GroupDashboardComponent } from './views/group-dashboard/group-dashboard/group-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestingServicesComponent
+    TestingServicesComponent,
+    ConnectionComponent,
+    GeneralDashboardComponent,
+    GroupDashboardComponent
   ],
   imports: [
     BrowserModule,
