4fd08c4edfd3403c0410a20e29a53911e9b23593
MathieuJJ <mathieu.jeanjoseph@gmail.com>
2021-06-29 11:56:28 +0200

    Work in progress on material bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,10 @@ import { TicketEditionComponent } from './views/group-dashboard/ticket-edition/t
 import { AddUserGroupComponent } from './views/group-dashboard/admin-options/add-user-group/add-user-group.component';
 import { GenericDialogComponent } from './views/commons/generic-dialog/generic-dialog.component';
 import { MatCardModule } from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import {FormBuilder} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -55,8 +59,12 @@ import { MatCardModule } from '@angular/material/card';
     BrowserAnimationsModule,
     HttpClientModule,
     MatCardModule,
+    MatFormFieldModule,
+    FormBuilder,
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill', floatLabel: 'always'}},
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/views/connection/connection.component.ts
+++ src/app/views/connection/connection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-connection',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connection.component.scss']
 })
 export class ConnectionComponent implements OnInit {
+  options: FormGroup;
+  hideRequiredControl = new FormControl(false);
+  floatLabelControl = new FormControl('auto');
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+    this.options = fb.group({
+      hideRequired: this.hideRequiredControl,
+      floatLabel: this.floatLabelControl,
+    });
+  }
 
   ngOnInit(): void {
   }
