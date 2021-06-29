4447f354add033351daf92f666de0c0f0cdd1b0a
Furvent <furventt@gmail.com>
2021-06-29 09:31:55 +0200

    Change tree view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestingServicesComponent } from './tests/testing-services/testing-services.component';
-import { ConnectionComponent } from './views/connection/connection/connection.component';
-import { GeneralDashboardComponent } from './views/general-dashboard/general-dashboard/general-dashboard.component';
-import { GroupDashboardComponent } from './views/group-dashboard/group-dashboard/group-dashboard.component';
+import { ConnectionComponent } from './views/connection/connection.component';
+import { GeneralDashboardComponent } from './views/general-dashboard/general-dashboard.component';
+import { GroupDashboardComponent } from './views/group-dashboard/group-dashboard.component';
 
 const routes: Routes = [
-  {path: 'tests', component: TestingServicesComponent},
-  {path: 'connection', component: ConnectionComponent},
-  {path: 'general-dashboard', component: GeneralDashboardComponent},
-  {path: 'group-dashboard', component: GroupDashboardComponent},
-  {path: '', redirectTo: 'connection', pathMatch: 'full'},
+  { path: 'tests', component: TestingServicesComponent },
+  { path: 'connection', component: ConnectionComponent },
+  { path: 'general-dashboard', component: GeneralDashboardComponent },
+  { path: 'group-dashboard', component: GroupDashboardComponent },
+  { path: '', redirectTo: 'connection', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
