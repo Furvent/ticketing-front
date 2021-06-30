d79d93aed016d656fed919f8a4fe62d53c64453b
Furvent <furventt@gmail.com>
2021-06-30 09:44:44 +0200

    WIP on group-dashboard component

--- src/app/views/group-dashboard/group-dashboard.component.ts
+++ src/app/views/group-dashboard/group-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GroupDashboardService } from 'src/services/group-dashboard/group-dashboard.service';
+import { UserService } from 'src/services/user/user.service';
+import { LoginForm } from 'src/shared/definitions/common';
 
 @Component({
   selector: 'app-group-dashboard',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupDashboardComponent implements OnInit {
 
-  constructor() { }
+  private mockedUserLoginForm: LoginForm = {
+    username: 'mimi',
+    password: 'pwdMimi',
+  };
+
+  private mockedGroupId = 1;
+
+  isLoadingData = true;
+
+  constructor(private userService: UserService, private groupService: GroupDashboardService) { }
 
   ngOnInit(): void {
+    this.userService.login(this.mockedUserLoginForm).then((response) => {
+      this.groupService.fetchGroupDashboardData(this.mockedGroupId).then((response) => {
+        this.isLoadingData = false;
+      });
+    });
   }
 
 }
