556acf9799ac503f565535816eb20381a4ea0114
Furvent <furventt@gmail.com>
2021-07-28 14:33:02 +0200

    Implement signup

--- src/app/views/connection/sign-up/sign-up.component.ts
+++ src/app/views/connection/sign-up/sign-up.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/services/user/user.service';
+import { NewUser } from 'src/shared/definitions/common';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  username: string = '';
+  password: string = '';
+  pseudo: string = '';
 
-  username: string = "";
-  password: string = "";
-  pseudo: string = "";
+  constructor(private userService: UserService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  signUp(): void {
+    const loginForm: NewUser = {
+      username: this.username,
+      password: this.password,
+      pseudo: this.pseudo,
+    };
+    this.userService
+      .signUp(loginForm)
+      .then(() => {
+        this.router.navigate(['general-dashboard']);
+      })
+      .catch((error) => {
+        console.error('Error when signIn: ', error);
+      });
   }
-
-  onSelect(): void{
-    console.log("this.username", this.username);
-    console.log("this.password", this.password);
-    console.log("this.pseudo", this.pseudo);
-  }
-
 }
