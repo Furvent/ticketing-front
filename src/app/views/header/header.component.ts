0765b9d195a6bf016f68dc709ea98a987a9ed89f
Roch-Jacquot <roch.jacquot@gmail.com>
2021-07-29 16:27:12 +0200

    profile modified

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GeneralDashboardService } from 'src/services/general-dashboard/general-dashboard.service';
@@ -8,10 +9,26 @@ import { UserService } from 'src/services/user/user.service';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('heightGrow', [
+        state('closed', style({
+            height: 0,
+        })),
+        state('open', style({
+          //height: 250
+        })),
+        transition('closed => open', animate(800)),
+        transition('open => closed', animate(800)), 
+    ]),
+    
+]
 })
 export class HeaderComponent implements OnInit {
   @Input()
   flagDisplayButtonBackToGeneralDashboard = false;
+
+  //isShowProfileDiv = true;
+  state = "closed";
 
   constructor(
     private userService: UserService,
@@ -36,5 +53,9 @@ export class HeaderComponent implements OnInit {
     this.gdService.resetData();
     this.userService.resetData();
     this.router.navigate(['connection']);
+  }
+  toggleDisplayProfileDiv(){
+    //this.isShowProfileDiv = !this.isShowProfileDiv;
+    (this.state == "closed") ? this.state = "open" : this.state = "closed";
   }
 }
